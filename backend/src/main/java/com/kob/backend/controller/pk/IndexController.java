54b6dd257ad99f54416c7dbaf425aa54bb360592
123fyq123 <1210915690@qq.com>
2023-06-28 15:56:18 +0800

    add web

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -3,13 +3,28 @@ package com.kob.backend.controller.pk;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
 @RequestMapping("/pk/")
 public class IndexController {
 
     @RequestMapping("index/")
     public String index() {
         return "pk/index.html";
+    }
+
+
+    @RequestMapping("getinfo/")
+    public String getInfo() {
+        String a = "aaa";
+        String b = "bbb";
+        List<String> list = new LinkedList<>();
+        list.add(a);
+        list.add(b);
+        return a;
     }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="nav">
-    hello world
+    <div>bot: {{a}}</div>
   </div>
   <router-view/>
 </template>
 
+<script>
+  import $ from 'jquery';
+  import { ref } from 'vue'
+
+  export default {
+    name: "APP",
+    setup: () => {
+      let a = ref("");
+      // let b = ref("");
+
+      $.ajax({
+        url: "http://127.0.0.1:3000/pk/getinfo/",
+        type: "get",
+        success: resp => {
+          a = resp;
+        }
+      });
+
+      return {
+        a
+      }
+    }
+
+  }
+</script>
 <style>
   body {
-    body {
-      background-image: url("/assets/background.png");
-    }
+    background-image: url("./assets/background.png");
+    background-size: cover;
   }
 </style>
