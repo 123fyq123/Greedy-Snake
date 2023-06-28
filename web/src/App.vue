ba1f15715f976bf26587fc644c3ac874f1620059
123fyq123 <1210915690@qq.com>
2023-06-28 15:20:38 +0800

    add backend

---
+++ web/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="nav">
+    hello world
+  </div>
+  <router-view/>
+</template>
+
+<style>
+  body {
+    body {
+      background-image: url("/assets/background.png");
+    }
+  }
+</style>
