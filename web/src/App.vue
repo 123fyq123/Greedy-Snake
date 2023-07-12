139472b373560c166e118d19790d4493505262a6
123fyq123 <1210915690@qq.com>
2023-07-12 22:15:31 +0800

    finish pagenation

--- web/src/App.vue
+++ web/src/App.vue
@@ -14,37 +14,7 @@ export default {
     NavBar
 },
 setup() {
-  
 
-  // $.ajax({
-  //   url: "http://127.0.0.1:3000/user/account/info/",
-  //   type: "GET",
-  //   Headers: {
-  //     Authorization: "Bearer token" + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlNGMwYWY5NTU4ZmQ0YWJkYTdmY2YxNmQ0NTI3YjM2ZSIsInN1YiI6IjMiLCJpc3MiOiJzZyIsImlhdCI6MTY4ODEzODkwMiwiZXhwIjoxNjg5MzQ4NTAyfQ.zmSep8_j1WmX-H7d2SyN3znzLVFmxNc6bRpTV9Hyelc"
-  //   },
-  //   success(resp) {
-  //     console.log(resp);
-  //   }, 
-  //   error(resp) {
-  //     console.log(resp);
-  //   }
-  // });
-
-  // $.ajax({
-  //   url: "http://127.0.0.1:3000/user/account/register/",
-  //   type: "post",
-  //   data: {
-  //     username: "fyq3",
-  //     password: "fyq3",
-  //     confirmedPassword: "fyq3",
-  //   },
-  //   success(resp) {
-  //     console.log(resp);
-  //   }, 
-  //   error(resp) {
-  //     console.log(resp);
-  //   }
-  // })
 }
 }
 </script>
@@ -52,5 +22,6 @@ setup() {
   body {
     background-image: url("./assets/images/background.png");
     background-size: cover;
+    user-select:none;
   }
 </style>
--- web/src/views/record/RecordIndexView.vue
+++ web/src/views/record/RecordIndexView.vue
@@ -30,6 +30,15 @@
                 </tr>
             </tbody>
         </table>
+        <nav aria-label="...">
+            <ul class="pagination" style="float: right;">
+                <li @click="click_page(-2)" class="page-item"><a class="page-link" href="#">前一页</a></li>
+                <li @click="click_page(page.number)" :class="'page-item ' + page.is_active" v-for="page in pages" :key="page.number">
+                    <a class="page-link" href="#">{{ page.number }}</a>
+                </li>
+                <li @click="click_page(-1)" class="page-item"><a class="page-link" href="#">后一页</a></li>
+            </ul>
+        </nav>
     </ContentField>
 </template>
 
@@ -49,8 +58,35 @@ export default {
         let current_page = 1;
         let records = ref([]);
         let total_records = 0;
+        let pages = ref([]);
 
-        console.log(total_records);
+        const click_page = page => {
+            if(page == -2) {
+                page = current_page - 1;
+            } else if(page == -1) {
+                page = current_page + 1;
+            } 
+
+            let max_pages = parseInt(Math.ceil(total_records / 10));
+            if(page >= 1 && page <= max_pages) {
+                pull_page(page);
+            }
+        }
+
+        const update_pages = () => {
+            let max_pages = parseInt(Math.ceil(total_records / 10));
+            let new_pages = [];
+            for(let i = current_page - 2; i <=current_page + 2; i ++ ) {
+                if(i >= 1 && i <= max_pages) {
+                    new_pages.push({
+                        number: i,
+                        is_active: i === current_page ? "active" : "",
+                    });
+                }
+            }
+            pages.value = new_pages;
+        }
+
         const pull_page = page => {
             current_page = page;
             $.ajax({
@@ -65,6 +101,7 @@ export default {
                 success(resp) {
                     records.value = resp.records;
                     total_records = resp.records_count;
+                    update_pages();
                 },
                 error(resp) {
                     console.log(resp);
@@ -119,6 +156,8 @@ export default {
         return {
             records,
             open_record_content,
+            pages,
+            click_page,
         }
     }
 }
